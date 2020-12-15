285d8017035c7fab0f3c29ca2330f184f4923f7d
Mohammad Al-Haddad <MohammadxCoder@icloud.com>
2020-12-15 17:12:05 +0300

    Search Artists

--- app.py
+++ app.py
@@ -182,7 +182,6 @@ def search_venues():
 @app.route('/venues/<int:venue_id>')
 def show_venue(venue_id):
     # shows the venue page with the given venue_id
-    # TODO: replace with real venue data from the venues table, using venue_id
     venue = Venue.query.get(venue_id)
     past_shows = []
     for show in venue.past_shows:
@@ -268,18 +267,21 @@ def artists():
 
 @app.route('/artists/search', methods=['POST'])
 def search_artists():
-    # TODO: implement search on artists with partial string search. Ensure it is case-insensitive.
-    # seach for "A" should return "Guns N Petals", "Matt Quevado", and "The Wild Sax Band".
-    # search for "band" should return "The Wild Sax Band".
+    search_term = request.form.get('search_term', '')
+    artists = Artist.query.filter(
+        Artist.name.ilike('%' + search_term + '%')).all()
+    data = []
+    for artist in artists:
+        data.append({
+            "id": artist.id,
+            "name": artist.name,
+            "num_upcoming_shows": artist.num_upcoming_shows
+        })
     response = {
-        "count": 1,
-        "data": [{
-            "id": 4,
-            "name": "Guns N Petals",
-            "num_upcoming_shows": 0,
-        }]
+        "count": len(data),
+        "data": data
     }
-    return render_template('pages/search_artists.html', results=response, search_term=request.form.get('search_term', ''))
+    return render_template('pages/search_artists.html', results=response, search_term=search_term)
 
 
 @app.route('/artists/<int:artist_id>')
