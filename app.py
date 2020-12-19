63085e5973b76786247074361274d8406065e602
Mohammad Al-Haddad <MohammadxCoder@icloud.com>
2020-12-20 02:41:20 +0300

    Added Delete on Cascade for Venue Show Relationship, added onDelete in the Foriegn Key

--- app.py
+++ app.py
@@ -46,7 +46,7 @@ class Venue(db.Model):
     seeking_talent = db.Column(db.Boolean)
     seeking_description = db.Column(db.String(500))
     shows = db.relationship('Show', backref='venue',
-                            lazy=True)
+                            lazy=True, cascade="all, delete-orphan")
 
     def __init__(self, name, city, state, address, phone, image_link, facebook_link, genres, website, seeking_talent, seeking_description, shows):
         self.name = name
@@ -134,7 +134,8 @@ class Artist(db.Model):
 class Show(db.Model):
     __tablename__ = 'show'
     id = db.Column(db.Integer, primary_key=True)
-    venue_id = db.Column(db.Integer, db.ForeignKey('venue.id'), nullable=False)
+    venue_id = db.Column(db.Integer, db.ForeignKey(
+        'venue.id', ondelete='CASCADE'), nullable=False)
     artist_id = db.Column(db.Integer, db.ForeignKey(
         'artist.id'), nullable=False)
     start_time = db.Column(db.DateTime, nullable=False,
